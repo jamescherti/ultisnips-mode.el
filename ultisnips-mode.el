035cacccebe1629eca5ce24fd6f597a4e5453d23
James Cherti <60946298+jamescherti@users.noreply.github.com>
2026-01-24 17:58:38 -0500

    Release: 1.0.1

--- ultisnips-mode.el
+++ ultisnips-mode.el
@@ -3,7 +3,7 @@
 ;; Copyright (C) 2025-2026 James Cherti | https://www.jamescherti.com/contact/
 
 ;; Author: James Cherti
-;; Version: 1.0.0
+;; Version: 1.0.1
 ;; URL: https://github.com/jamescherti/ultisnips-mode.el
 ;; Keywords: languages
 ;; Package-Requires: ((emacs "26.3"))
